6b5319a82437a72dd94e785102d58fa23ac2df66
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-11-08 16:28:44 +0530

    Object iterations

---
+++ iterateObjects.js
@@ -0,0 +1,27 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDairy() {
+    for (var i of dairy)
+        console.log(i);
+}
+logDairy();
+
+// Task 2
+const animal = {
+canJump: true
+};
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+function birdCan() {
+    for (key of Object.keys(bird))
+        console.log(key + ": " + bird[key]);
+}
+birdCan()
+
+// Task 3
+function animalCan() {
+   for (key in bird)
+        console.log(key + ": " + bird[key]);  
+}
+animalCan();
