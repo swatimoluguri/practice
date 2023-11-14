31ba60c1b4103af34970f5a4169b66de1a06f233
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-11-14 20:39:35 +0530

    working with map

---
+++ Leetcode/day1.js
@@ -0,0 +1,19 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * 
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (numMap.has(complement)) {
+        return [numMap.get(complement), i];
+      }
+      numMap.set(nums[i], i);
+    }
+    return null;
+  }
